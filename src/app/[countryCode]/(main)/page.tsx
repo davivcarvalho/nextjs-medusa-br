93bc945d1500c7f13490f94433250a556fbe19b8
Davi Carvalho <caminhounico@gmail.com>
2024-03-23 16:26:45 -0300

    changed banner image and style

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -69,7 +69,7 @@ export default async function Home({
   return (
     <>
       <Hero />
-      <div className="py-12">
+      <div className="py-6">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
         </ul>
--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,9 +2,12 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className="h-2/6 w-full border-b border-ui-border-base bg-ui-bg-subtle flex justify-center">
-      <Image src={require('../../../../../public/home-bg.jpg')} width={1024} height={1024} className="flex items-center justify-center" alt=""/>
-
+    <div className="relative w-full pb-[67%] border-b border-ui-border-base bg-ui-bg-subtle">
+      <Image src="/images/home-bg.jpg" 
+        width={1024}
+        height={1024}
+        alt="Banner Image"
+        className="absolute inset-0 object-cover"/>
     </div>
   )
 }
