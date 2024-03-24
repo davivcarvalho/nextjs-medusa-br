7baf98f3bb5624ec0de3bfa1fd8d6e7a7b7e51e2
Davi Carvalho <caminhounico@gmail.com>
2024-03-24 17:32:23 -0300

    fix banner image responsible

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className="relative w-full pb-[67%] border-b border-ui-border-base bg-ui-bg-subtle">
+    <div className="w-full border-b border-ui-border-base bg-ui-bg-subtle">
       <Image src="/images/home-bg.jpg" 
-        width={1024}
+        width={2048}
         height={1024}
         alt="Banner Image"
-        className="absolute inset-0 object-cover"/>
+        className="max-h-96 inset-0 object-cover"/>
     </div>
   )
 }
